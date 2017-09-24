1adfbd6bd74e6991c7630146e581e5930098f0f7
kan zhang <kz233@afsconnect1.njit.edu>
2017-09-24 01:57:09 -0400

    add basic structure

--- arr_fun.php
+++ arr_fun.php
@@ -1,14 +1,32 @@
 <?php
-	echo '<h1 style="color:blue;font-size:40;font-family:calibri;">10 Array
-	functions Practice</h1>';
+	function h($stuff){
+		echo "<li><h2>$stuff</h2></li>";
+	}
+	function r($stuff){
+		echo "<blockquote>$stuff</blockquote><hr>";
+	}
+	echo '<h1 style="color:blue;font-size:40;font-family:calibri;">10 Array functions Practice</h1>';
 	echo '<ul>';
-	echo '<li>First function</li>';
-	echo '<blockquote>Result</blockquote>';
-	echo '<hr>';
-	echo '</ul>';	
+
+	//first
+	h("first function");
+	r("result");
+
+	//second
+	h("second fuction");
+	r("result");
+
+	echo '</ul>';
+	class main{
+
+		public function a(){
+		echo "there ate textXXXX";	
+		}
 	
-
-
+		public function b(){
+		echo "nonono";
+		}
+	}
 ?>
 	
 
