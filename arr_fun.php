f5738379b627de26e7521169421ee793dc8a92af
kan zhang <kz233@afsconnect1.njit.edu>
2017-09-24 00:53:25 -0400

    initialization

---
+++ arr_fun.php
@@ -0,0 +1,14 @@
+<?php
+	echo '<h1 style="color:blue;font-size:40;font-family:calibri;">10 Array
+	functions Practice</h1>';
+	echo '<ul>';
+	echo '<li>First function</li>';
+	echo '<blockquote>Result</blockquote>';
+	echo '<hr>';
+	echo '</ul>';	
+	
+
+
+?>
+	
+
