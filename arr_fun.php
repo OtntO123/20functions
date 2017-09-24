394aa02a21e28414ea51f7a5e124882167fabf55
kan zhang <kz233@afsconnect1.njit.edu>
2017-09-24 15:28:43 -0400

    finally pushed 1oth func

--- arr_fun.php
+++ arr_fun.php
@@ -1,7 +1,7 @@
 <?php
 	$OB= new main();
 	//first
-	$OB -> e1('array_change_key_case()');
+	$OB -> e1('1.array_change_key_case()');
 	$arr = array("Aca" => 1, "csaA" => 2);	
 	$OB -> P2($arr);
 	$OB -> e2('To upper_case');
@@ -11,7 +11,7 @@
 	$OB -> e3();
 
 	//second
-	$OB -> e1('array_chunk()');
+	$OB -> e1('2.array_chunk()');
 	$ar = array('1a','2a','3a'.'4a'.'5a');
 	$OB -> P2($ar);
 	$OB -> e2('chunk to 2');
@@ -19,7 +19,7 @@
 	$OB -> e3();
 
 	//3rd
-	$OB -> e1('array_column()');
+	$OB -> e1('3.array_column()');
 	$ar = array(
 			array(
 	            	'No' => 1,
@@ -49,7 +49,7 @@
 	$OB -> e3();
 
 	//4th
-	$OB -> e1('array_combine');
+	$OB -> e1('4.array_combine()');
 	$ar1 = array('Hey', 'What\'s', 'It\'s');
 	$ar2 = array('man', 'up', 'mine');
 	$OB -> P2($ar1);
@@ -59,7 +59,7 @@
 	$OB -> e3();
 
 	//5th
-	$OB -> e1('array_count_values()');
+	$OB -> e1('5.array_count_values()');
 	$ar = array(1,4,4,4,55,55,66,6,1,1,1,1,1,1,1,'w','w','w','w','w','d','g','b','ff','ff');
 	$OB -> P2($ar);
 	$OB -> e2('Result');
@@ -67,7 +67,7 @@
 	$OB -> e3();
 
 	//6th
-	$OB -> e1('array_diff_assoc');
+	$OB -> e1('6.array_diff_assoc()');
 	$ar1 = array('c'=>'apple', 'd' => 'dark', 0, 'good');
 	$ar2 = array('c'=>'apple', 'dark', 'good','0'=>0);
 	$OB -> P2($ar1);
@@ -76,7 +76,43 @@
 	$OB -> P2( $OB -> DifAss($ar1, $ar2));
 	$OB -> e3();
 
+	//7th
+	$OB -> e1('7.array_diff_key()');
+	$OB -> P2($ar1);
+	$OB -> P2($ar2);
+	$OB -> e2('Result');
+	$OB -> P2( $OB -> DifKey($ar1, $ar2));
+	$OB -> e3();
 
+	//8th
+	$OB -> e1('8.array_fill_keys()');
+	$ar = array('black', 1, 'first');
+	$OB -> P2($ar);
+	$OB -> e2('Result');
+	$OB -> P2( $OB -> FilKey($ar, 'bank account'));
+	$OB -> e3();
+
+	//9th
+	$OB -> e1('9.array_map()');
+	$ar1 = array(1, 2, 3, 4, 5, 6);
+	$ar2 = array("pen", "apple", "applepen", "pineapple", "pen","pineappleapplepen");
+	$OB -> e2('Result');	
+	//function ArrMap($a, $b){
+	//	return ("{$a}. We need $b");
+	//	}
+	$OB -> ArrMap($ar1, $ar2);
+	$OB -> e3();
+
+	//10th
+	$OB -> e1('10.array_push()');
+	$ar = array("Apple", "pen");
+	$OB -> P2($ar);
+	$OB -> e2('Result');
+	$OB -> ArrPus($ar, "applepan");
+	$OB -> e3();
+	
+
+	
 	echo '</ul>';
 	
 	class main{		
@@ -122,6 +158,23 @@
 
 		public function DifAss($a, $b){
 			return array_diff_assoc($a, $b);
+		}
+
+		public function DifKey($a, $b){
+			return array_diff_key($a, $b);
+		}
+	
+		Public function FilKey($a, $b){
+			return array_fill_keys($a, $b);
+		}
+
+		public function ArrMap($a, $b){
+			print_r(array_map(null, $a, $b));
+		}
+	
+		public function ArrPus($a,$b){
+			array_push($a, $b);
+			print_r($a);
 		}
 
 		public function __destruct(){}
