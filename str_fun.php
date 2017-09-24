3784fba1d199a8cd18f89a7506e921c78f57490f
kan zhang <kz233@afsconnect1.njit.edu>
2017-09-24 17:50:01 -0400

    structured

---
+++ str_fun.php
@@ -0,0 +1,34 @@
+<?PHP
+	$OB= new main();
+	//1st
+	$OB -> e1('');
+	$st = ;
+	$OB -> e2('Result');
+	$OB -> e3;
+
+
+
+
+	echo '</ul>';
+
+	class main{
+	public function __construct(){
+		echo '<h1 style="color:blue;font-size:40;font-family:calibri;">10 String
+		functions Practice</h1><ul>';
+		}
+	public function e1($a){
+		echo "<li><h2>$a</h2></li>";	
+		}
+	Public function e2($a){
+		echo "<blockquote>$stuff</blockquote>";
+		}
+	public function e3(){
+		echo "<hr>";
+		}
+		
+
+
+
+	public function __destruct(){}
+	}
+?>
