81a30018913da7360aa9ebd3f3f200cf3f6e2b73
kan zhang <kz233@afsconnect1.njit.edu>
2017-09-24 11:54:46 -0400

    second func

--- arr_fun.php
+++ arr_fun.php
@@ -1,31 +1,56 @@
 <?php
-	function h($stuff){
-		echo "<li><h2>$stuff</h2></li>";
-	}
-	function r($stuff){
-		echo "<blockquote>$stuff</blockquote><hr>";
-	}
-	echo '<h1 style="color:blue;font-size:40;font-family:calibri;">10 Array functions Practice</h1>';
-	echo '<ul>';
-
+	$OB= new main();
 	//first
-	h("first function");
-	r("result");
+	$OB -> e1('array_change_key_case()');
+	$arr = array("Aca" => 1, "csaA" => 2);	
+	$OB -> P2($arr);
+	$OB -> e2('To upper_case');
+	$OB -> P2( $OB-> ChgKeyCas($arr, CASE_UPPER));
+	$OB -> e2('To lower_case');
+	$OB -> P2( $OB-> ChgKeyCas($arr, CASE_LOWER));
+	$OB -> e3();
 
 	//second
-	h("second fuction");
-	r("result");
+	$OB -> e1('array_chunk()');
+	$ar = array('1a','2a','3a'.'4a'.'5a');
+	$OB -> P2($ar);
+	$OB -> e2('chunk to 2');
+	$OB -> P2( $OB -> Chu($ar, 2));
+	$OB -> e3();
 
+	
 	echo '</ul>';
-	class main{
+	
+	class main{		
+		public function __construct(){
+                	echo '<h1 style="color:blue;font-size:40;font-family:calibri;">10 Array functions Practice</h1><ul>';
+		}
 
-		public function a(){
-		echo "there ate textXXXX";	
+		public function e1($stuff){
+			echo "<li><h2>$stuff</h2></li>";
+		}
+		
+		public function e2($stuff){
+			echo "<blockquote>$stuff</blockquote>";
+		}
+
+		public function e3(){
+			echo "<hr>";
+		}
+
+		public function P2($tem){
+			print_r($tem);
 		}
 	
-		public function b(){
-		echo "nonono";
+		public function ChgKeyCas($tem, $tem2){
+			return array_change_key_case($tem, $tem2);
 		}
+	
+		public function Chu($a,$b){
+			return array_chunk($a,$b);
+		}
+		
+		Public function __destruct(){}
 	}
 ?>
 	
