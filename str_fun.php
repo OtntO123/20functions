d16b9f2da9c6804041a9a38be7a7d371218a6c2f
kan zhang <kz233@afsconnect1.njit.edu>
2017-09-24 20:24:51 -0400

    minimized codwe

--- str_fun.php
+++ str_fun.php
@@ -1,20 +1,11 @@
 <?PHP
-	$O= new main();
+	$OB= new main();
 	//1st
-	$O -> e1('addcslashes()');
-	$s = '1,al /?\'h[s{g]g|`s!ad';
-	$O -> e2($s);
-	$O -> e2('Result');
-	$O -> e2( $O -> AddSla($s, 'A..z'));
-	$O -> e3();
+	$OB -> AddSla('1w [w]1', 'w');
 
-        //
-	$O -> e1('addcslashes()');
-	$s = '1,al /?\'h[s{g]g|`s!ad';
-	$O -> e2($s);
-	$O -> e2('Result');
-	$O -> e2();
-	$O -> e3();
+
+
+
 
 
 
@@ -25,19 +16,17 @@
 		echo '<h1 style="color:blue;font-size:40;font-family:calibri;">10 String
 		functions Practice</h1><ul>';
 		}
-	public function e1($a){
-		echo "<li><h2>$a</h2></li>";	
+	public function e($d, $e, $f){
+		echo "<li><h2>$d</h2></li>";
+		echo "<blockquote>$e</blockquote>";
+		echo "<blockquote>Result</blockquote>";
+		echo "<blockquote>$f</blockquote><hr>";
 		}
-	Public function e2($a){
-		echo "<blockquote>$stuff</blockquote>";
+	
+	public function AddSla($a, $b){	
+		$this -> e('addcslashed()', $a, addcslashes($a, $b));
 		}
-	public function e3(){
-		echo "<hr>";
-		}
-	public function AddSla($a, $b){
-		return addcslashes($a, $b);
-		}
-		
+	
 
 
 
